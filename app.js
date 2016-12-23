30035f5da0649b73afa7690eaec9e7bf9c0b4c89
Sudheesh <sudheesh@beesys.com>
2016-12-23 11:39:10 +0530

    update

--- app.js
+++ app.js
@@ -5,22 +5,24 @@ var server =restify.createServer();
 server.listen(process.env.PORT || 1976,function()
 {
     console.log('% Listenting to %s',server.name,server.url);
-   
 }
 );
-
 // Create chat bot
 var connector = new builder.ChatConnector
 ({appid: process.env.MY_APP_ID,appsecret: process.env.MY_APP_SECRET }); 
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector,function(session)
+{
+    session.send("You said: %s", session.message.text);
+}
+);
 server.post('/api/messages', connector.listen());
 
-server.get('/', restify.serveStatic({
+/*server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
-}));
+}));*/
 
 bot.dialog('/',function(session)
 {
-    session.send("Hi Iam Personal Assiatnt to Sudheesh. May i Help You");
+    session.send("You said: %s", session.message.text);
 });
