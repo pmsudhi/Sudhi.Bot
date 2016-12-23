a8a628fa73ecf4f49a8fd7370241db3133bda4b9
Sudheesh <sudheesh@beesys.com>
2016-12-23 17:29:32 +0530

    updates

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ var server =restify.createServer();
 server.listen(process.env.PORT || 1976,function()
 {
     console.log('% Listenting to %s',server.name,server.url);
+     console.log('APPID: %s',process.env.MY_APP_ID);
+     console.log('appsecret: %s',process.env.MY_APP_SECRET);
 }
 );
 // Create chat bot
 var connector = new builder.ChatConnector
-({appid: process.env.MY_APP_ID,appsecret: process.env.MY_APP_SECRET }); 
-var bot = new builder.UniversalBot(connector,function(session)
-{
-    session.send("You said: %s", session.message.text);
-}
-);
-server.post('/api/messages', connector.listen());
+(
+    
+   // {appid: null,appsecret: null }
+   {appid: process.env.MY_APP_ID,appPassword: process.env.MY_APP_SECRET }
+); 
+var bot = new builder.UniversalBot(connector);
 
-/*server.get('/', restify.serveStatic({
+server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
-}));*/
+}));
+
+server.post('/api/messages', connector.listen());
+
 
 bot.dialog('/',function(session)
 {
